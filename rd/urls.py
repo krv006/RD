dd6de0ea4f394f8916f9e6504301b55a7584e3c2
“krv006” <rvkamronbek@gmail.com>
2024-06-29 18:43:55 +0500

    ckditor

--- rd/urls.py
+++ rd/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('apps.urls')), 
+    path('api/', include('apps.urls')), 
     path("ckeditor5/", include('django_ckeditor_5.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
