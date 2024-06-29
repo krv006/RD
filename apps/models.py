d560f42490e59b634fd4ca14cbb426d806845b49
“krv006” <rvkamronbek@gmail.com>
2024-06-29 12:25:37 +0500

    models

--- apps/models.py
+++ apps/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     language = models.CharField(max_length=2, choices=LANGUAGE_CHOICES, default='uz')
     url = models.URLField()
     thumb = models.ImageField(upload_to='thumbs/')
-    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, null=True, blank=True)
     date = models.DateField(auto_now_add=True)
     doi = models.CharField(max_length=200,blank=True,null=True)
     best = models.BooleanField(default=False)
@@ -30,16 +30,7 @@ class Product(models.Model):
         return self.name
 
 
-class Contact(models.Model):
-    univercity_name = models.CharField(max_length=255)
-    phone_number = models.CharField(max_length=255)
-    location = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.univercity_name
-
-
-class About(models.Model):
+class Contact_us(models.Model):
     address = models.CharField(max_length=1000)
     email = models.CharField(max_length=1000)
     phone_number = models.CharField(max_length=1000)
@@ -47,19 +38,6 @@ class About(models.Model):
     def __str__(self):
         return self.address
 
-
-class Worker(models.Model):
-    full_name = models.CharField(max_length=50)
-    work = models.CharField(max_length=55)
-    image = models.ImageField(upload_to='workers/image')
-
-    def __str__(self):
-        return self.full_name
-
-
-class Social_media(models.Model):
-    icon = models.CharField(max_length=255)
-    url = models.URLField()
 
 class Partner(models.Model):
     image = models.ImageField(upload_to='thumbs/partners')
@@ -69,16 +47,12 @@ class Partner(models.Model):
         return self.url
 
 
-#? RALED SAYTIDAN KORIB YANGI NARSALARDI QOSHISH KERAK
-
-
-
 class Redikt(models.Model):
     name= models.CharField(max_length=255)
-    work_place_uz = models.TextField()
-    work_place_ru = models.TextField()
-    work_place_en = models.TextField()
-    discription = models.TextField(blank=True,null=True)
+    description_place_uz = models.TextField()
+    description_place_ru = models.TextField()
+    description_place_en = models.TextField()
+    image = models.ImageField(upload_to='editors')
 
     def __str__(self):
         return self.name
